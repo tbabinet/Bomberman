6e021b473003b9c2fbe6c5bdffc3afeac42055b8
romain.josse <romain.josse@hotmail.fr>
2019-03-24 19:17:37 +0100

    Affichage FPS -> à revoir

--- main.js
+++ main.js
@@ -7,6 +7,22 @@ let init = async function () {
     let c = new Personnage(l);
     
     let bombList = new Array();
+    
+    // Pour l'affichage des FPS :
+
+    // Récuperation de la checkbox du mode DEBUG
+    let input_debug = document.getElementById("debug_mode");
+    // Checkbox non coché par defaut
+    input_debug.checked = false;
+
+    // Récuperation de la div d'informations de debogage,
+    // par défaut non affiché
+    let info_debug = document.getElementById("info_debug");
+    info_debug.style.display = "none";
+
+    // Temps à l'initialisation
+    let init_time = new Date();
+    let fps = 0;
 
     window.addEventListener('keydown', function f (e) {
         if(e.keyCode==38 && c.posY>0){      
@@ -61,8 +77,29 @@ let init = async function () {
         //window.removeEventListener('keydown');
     });
 
-    
+    input_debug.addEventListener("change", ev => {
+        console.log(ev.target.checked);
+
+        if (ev.target.checked) {
+            console.log("Checked");
+            info_debug.style.display = "";
+        } else {
+            console.log("Unchecked");
+            info_debug.style.display = "none";
+        }
+    });
+
+    function updateFps(fps) {
+        info_debug.textContent = fps.toFixed(1) + " FPS";
+    }
+
     function draw() {
+        // FPS
+        let current_time = new Date();
+        fps = 1000 / (current_time - init_time);
+        init_time = current_time;
+        //updateFps(fps);
+        
         bombList = bombList.filter(b=>b.decompteExplosion>0);
         c.level = l;
         drawer.drawLevel(l);
@@ -73,7 +110,11 @@ let init = async function () {
         requestAnimationFrame(draw);
     }
 
-    window.requestAnimationFrame(draw)
+    window.requestAnimationFrame(draw);
+    
+    setInterval(() => {
+        updateFps(fps);
+    }, 500);
         
 }
 
